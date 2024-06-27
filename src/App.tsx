eb3ea6b3c3c3e8cdeda84b77dde4306f76911de3
Leonardo Gobetti <leonardogobetti@hotmail.com>
2024-06-27 12:23:44 -0300

    Criação componente Sobre e Catalogo

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import Sobre from './pages/sobreNos/Sobre';
+import Catalogo from './pages/catalogo/Catalogo';
 
 function App() {
   return (
@@ -10,7 +12,10 @@ function App() {
   
     <Navbar/>
     <Home/>
+    <Sobre/>
+    <Catalogo/>
     <Footer/>
+    
     
  
     </>
---
+++ src/pages/catalogo/Catalogo.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import logo from '../../assets/home.png'
+
+function Catalogo() {
+  return (
+    <>
+    <header>
+        <div className='flex '>
+            <h1>Catalogo</h1>
+            <div className='flex justify-center'>
+            <img className='w-2/3' src={logo} alt="" />
+
+            </div>
+            
+        </div>
+
+
+    </header>
+    <div>
+        
+
+    </div>
+    
+    </>
+    
+  )
+}
+
+export default Catalogo
